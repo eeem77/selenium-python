c8685fc8ab54c0a870a06c5969ed12af453f3b06
eeem77 <escobarmernestoe@gmail.com>
2024-01-25 07:22:43 -0400

    update

--- era.py
+++ era.py
@@ -3,10 +3,11 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 proxy = Proxy({ 
     'proxyType': ProxyType.MANUAL, 
-    'httpProxy': '117.160.250.133:8899', 
+    'httpProxy': '89.43.31.134:3128', 
     'ftpProxy': '', 
     'sslProxy': '', 
     'noProxy': '' # set this value as per your requirement 
@@ -17,34 +18,32 @@ chrome_options.add_argument('--proxy-server={0}'.format(proxy.httpProxy))
  
 driver = webdriver.Firefox(chrome_options) 
 
-driver.get(f"https://www.era.com/city/ca/san-mateo/agents")
-driver.implicitly_wait(30)
 
-for x in range(1,4):
+for x in range(1,2):
 
-    name = driver.find_elements(By.CSS_SELECTOR,"h3.MuiTypography-root.MuiTypography-subtitle1_md.css-1nj5942")
-    subtitle = driver.find_elements(By.CSS_SELECTOR,"span.MuiTypography-root.MuiTypography-body6.css-14q0kqp")
-    serial = driver.find_elements(By.CSS_SELECTOR,"span.MuiTypography-root.MuiTypography-body6.css-q6wjqd")
-    email = driver.find_elements(By.CSS_SELECTOR,"p.MuiTypography-root.MuiTypography-body2.css-kp2fog")
-    phone = driver.find_elements(By.CSS_SELECTOR,"p.MuiTypography-root.MuiTypography-body2.css-kp2fog")
-    btn = driver.find_element(By.CSS_SELECTOR,"svg.MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium.MuiPaginationItem-icon.css-n8417t-MuiSvgIcon-root-MuiPaginationItem-icon")
-
-    i = 0
-
-    for element in name:
-        if(not serial[i].text):
-            serialAgent = ''
-        else:
-            serialAgent = serial[i].text
-        agent = [element.text, subtitle[i].text, serialAgent, email[i].text, phone[i].text]
-        
-        with open('data.txt', 'a') as f:
-            f.write(f'\n{agent}')
-        print(agent)
-        i = i + 1
-
-    btn.click()
+    driver.get(f"https://www.era.com/city/ca/san-mateo/agents?page={x}")
     driver.implicitly_wait(30)
+
+    blockone = driver.find_elements(By.CSS_SELECTOR,"div.MuiStack-root.css-ea5hw0-MuiStack-root")
+
+    
+    for element in blockone:
+        print(element.find_element(By.CSS_SELECTOR, "span.MuiTypography-root.MuiTypography-body6.css-q6wjqd").is_displayed())
+    # name = driver.find_elements(By.CSS_SELECTOR,"h3.MuiTypography-root.MuiTypography-subtitle1_md.css-1nj5942")
+    # subtitle = driver.find_elements(By.CSS_SELECTOR,"span.MuiTypography-root.MuiTypography-body6.css-14q0kqp")
+    # serial = driver.find_elements(By.CSS_SELECTOR,"span.MuiTypography-root.MuiTypography-body6.css-q6wjqd")
+    # email = driver.find_elements(By.CSS_SELECTOR,"p.MuiTypography-root.MuiTypography-body2.css-kp2fog")
+    # phone = driver.find_elements(By.CSS_SELECTOR,"p.MuiTypography-root.MuiTypography-body2.css-kp2fog")
+    # i = 0
+
+    # for element in name:
+
+    #     agent = [element.text, subtitle[i].text, serial[i].text, email[i].text, phone[i].text]
+
+    #     with open('data.txt', 'a') as f:
+    #         f.write(f'\n{agent}')
+    #     print(agent)
+    #     i = i + 1
 
 driver.close()
 
